3dea8eb465bed4f4e50bed48b05060ed0672fed0
Mark Agustin <magustin0329@gmail.com>
2024-05-05 16:18:03 -0700

    Cloud build (#1)
    * adding cloud build
    
    * fixup
    
    * fix up
    
    * fix up
    
    * fix up

---
+++ terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID to host the cluster in"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to host the cluster in"
+}
+
+variable "repo_name" {
+  type        = string
+  description = "Name of the repo"
+}
+
+variable "connection_name" {
+  type        = string
+  description = "Name of the connector"
+}
+
+variable "repo_url" {
+  type        = string
+  description = "url of the repository"
+}
+
+variable "branch_name" {
+  type        = string
+  description = "Branch name"
+  default     = "^main$"
+}
+
+variable "pull_request_cloud_build_file" {
+  type        = string
+  description = "path to cloud build file"
+  default     = "cloud-build/cloudbuild-pull-request.yaml"
+}
+
+variable "push_cloud_build_file" {
+  type        = string
+  description = "path to cloud build file"
+  default     = "cloud-build/cloudbuild-push.yaml"
+}
