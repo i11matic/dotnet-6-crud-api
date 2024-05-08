2b74e4405c9a06d91e272a36da64690f05e1600b
Mark Agustin <magustin0329@gmail.com>
2024-05-07 19:34:24 -0700

    added provider info

--- terraform/version.tf
+++ terraform/version.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "carlpett/sops"
       version = "1.0.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
   required_version = ">= 0.13"
 }
